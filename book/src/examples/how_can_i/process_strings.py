5016d98a9364c9f7bfff6cb6336b1d314a9f5e8c
ritchie46 <ritchie46@gmail.com>
2020-12-15 08:20:06 +0100

    how can i process strings

---
+++ book/src/examples/how_can_i/process_strings.py
@@ -0,0 +1,19 @@
+import pypolars as pl
+from pypolars.lazy import *
+
+df = pl.DataFrame({"shakespeare": "All that glitters is not gold".split(" ")})
+
+str_lengths = df.lazy().with_column(
+    col("shakespeare").str_lengths().alias("letter_count")
+)
+
+df = pl.DataFrame({"a": "The man that ate a whole cake".split(" ")})
+
+filtered = df.lazy().filter(col("a").str_contains(r"(?i)^the$|^a$").is_not())
+
+if __name__ == "__main__":
+    with open("book/src/outputs/how_can_i_process_strings.txt", "w") as f:
+        f.write(str(str_lengths.collect()))
+
+    with open("book/src/outputs/how_can_i_process_strings_1.txt", "w") as f:
+        f.write(str(filtered.collect()))
--- book/src/reference.md
+++ book/src/reference.md
@@ -3,4 +3,5 @@
 Need to see all available methods/ functions of Polars? The reference guide is your best bet.
 
 * [Python](https://ritchie46.github.io/polars/pypolars/index.html)
-* [Rust](https://ritchie46.github.io/polars/polars/index.html)
+* [Rust master](https://ritchie46.github.io/polars/polars/index.html)
+* [Rust release](https://docs.rs/polars/)
