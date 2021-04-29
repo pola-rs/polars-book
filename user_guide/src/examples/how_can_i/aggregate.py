65f1abfb848975a816d8208ae4fcacdcd6a84f7c
Clement Arnarez <11967125+carnarez@users.noreply.github.com>
2021-04-29 20:45:36 +0200

    Contributing templates (#16)
    Restructure book
    
        Added a CONTRIBUTION.md guide, including code example template
        Renamed the book to user_guide
        Bump Polars version
        Deleted obsolete and superfluous files
        Introduced the pre-commit (and added a .pre-commit-config.yaml)
        Added a Dockerfile to build the environment

---
+++ user_guide/src/examples/how_can_i/aggregate.py
@@ -0,0 +1,11 @@
+import polars as pl
+from polars.lazy import *
+
+reddit = pl.scan_csv("data/reddit.csv").select(
+    [pl.sum("comment_karma"), pl.min("link_karma")]
+)
+
+if __name__ == "__main__":
+    df = reddit.fetch()
+    with open("user_guide/src/outputs/how_can_i_aggregate.txt", "w") as f:
+        f.write(str(df))
---
+++ user_guide/src/examples/how_can_i/process_strings.py
@@ -0,0 +1,19 @@
+import polars as pl
+from polars.lazy import col
+
+df = pl.DataFrame({"shakespeare": "All that glitters is not gold".split(" ")})
+
+str_lengths = df.lazy().with_column(
+    col("shakespeare").str_lengths().alias("letter_count")
+)
+
+df = pl.DataFrame({"a": "The man that ate a whole cake".split(" ")})
+
+filtered = df.lazy().filter(col("a").str_contains(r"(?i)^the$|^a$").is_not())
+
+if __name__ == "__main__":
+    with open("user_guide/src/outputs/how_can_i_process_strings.txt", "w") as f:
+        f.write(str(str_lengths.collect()))
+
+    with open("user_guide/src/outputs/how_can_i_process_strings_1.txt", "w") as f:
+        f.write(str(filtered.collect()))
