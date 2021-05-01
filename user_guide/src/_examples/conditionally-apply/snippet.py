089528c05cb22c2fe48002fb66fd2f25d6244ff3
carnarez <{ID}+{username}@users.noreply.github.com>
2021-05-01 13:08:47 +0200

    Final tweaks and linting

--- user_guide/src/_examples/conditionally-apply/snippet.py
+++ user_guide/src/_examples/conditionally-apply/snippet.py
@@ -1,15 +1,15 @@
 import numpy as np
 import polars as pl
 
-src_df = pl.DataFrame(
+dataset = pl.DataFrame(
     {"range": np.arange(10), "left": ["foo"] * 10, "right": ["bar"] * 10}
 )
 
-dataset = src_df.lazy().with_column(
+q = dataset.lazy().with_column(
     pl.when(pl.col("range") >= 5)
     .then(pl.col("left"))
     .otherwise(pl.col("right"))
     .alias("foo_or_bar")
 )
 
-df = dataset.collect()
+df = q.collect()
