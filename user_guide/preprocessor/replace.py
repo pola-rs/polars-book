9ef889ffe06d49d115a0f28cade9da6b5bf46c69
Ritchie Vink <ritchie46@gmail.com>
2021-06-06 15:22:02 +0200

    dt namespace link

--- user_guide/preprocessor/replace.py
+++ user_guide/preprocessor/replace.py
@@ -10,9 +10,9 @@ context, book = json.load(sys.stdin)
 content = json.dumps(book)
 
 substitutes = (
+    ("POLARS_PY_REF_GUIDE_V2", "https://pola-rs.github.io/polars-book/api-python"),
     ("POLARS_RS_REF_GUIDE", "https://docs.rs/polars"),
     ("POLARS_PY_REF_GUIDE", "https://pola-rs.github.io/polars/python/polars"),
-    ("POLARS_PY_REF_GUIDE_V2", "https://pola-rs.github.io/polars-book/api-python"),
 )
 
 for old, new in substitutes:
