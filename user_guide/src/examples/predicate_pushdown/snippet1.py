463099b70f97f1bbfe613fb946d3f485ff4f38a1
Rich <jychen7@users.noreply.github.com>
2021-12-28 17:16:32 -0500

    Update snippet1.py

--- user_guide/src/examples/predicate_pushdown/snippet1.py
+++ user_guide/src/examples/predicate_pushdown/snippet1.py
@@ -2,7 +2,7 @@ import polars as pl
 
 from ..paths import DATA_DIR
 
-q = (
+q1 = (
     pl.scan_csv(f"{DATA_DIR}/reddit.csv")
     .filter(pl.col("comment_karma") > 0)
     .filter(pl.col("link_karma") > 0)
