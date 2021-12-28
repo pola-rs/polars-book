35df5e821df43e31402f49a17958e6a9cee52b39
Rich <jychen7@users.noreply.github.com>
2021-12-28 17:15:40 -0500

    Update __main__.py

--- user_guide/src/examples/predicate_pushdown/__main__.py
+++ user_guide/src/examples/predicate_pushdown/__main__.py
@@ -1,7 +1,5 @@
 from ..paths import OUTPUT_BASE_DIR, create_if_not_exists
-from .snippet1 import df1
 from .snippet1 import q as q1
-from .snippet2 import df as df2
 from .snippet2 import q as q2
 
 path = create_if_not_exists(f"{OUTPUT_BASE_DIR}/predicate_pushdown")
@@ -10,6 +8,8 @@ for n, x in enumerate([q1, q2]):
     x.show_graph(optimized=False, show=False, output_path=f"{path}/graph{n + 1}.png")
     x.show_graph(optimized=True, show=False, output_path=f"{path}/graph{n + 1}-optimized.png")
 
+df1 = q1.fetch(int(1e7))
+df2 = q2.fetch(int(1e7))
 for n, x in enumerate([df1, df2]):
     with open(f"{path}/output{n + 1}.txt", "w") as f:
         f.write(f"{x}\n")
