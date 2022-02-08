d0b44804acf9f34d2ffdf395dcc499db2323985d
Ritchie Vink <ritchie46@gmail.com>
2022-02-08 11:56:20 +0100

    update root (#123)

--- user_guide/preprocessor/replace.py
+++ user_guide/preprocessor/replace.py
@@ -10,7 +10,7 @@ context, book = json.load(sys.stdin)
 content = json.dumps(book)
 
 substitutes = (
-    ("POLARS_ROOT", "/polars-book"),
+    ("POLARS_ROOT", "/polars-book/user-guide"),
     ("POLARS_RS_REF_GUIDE", "https://docs.rs/polars"),
     (
         "POLARS_PY_REF_GUIDE",
