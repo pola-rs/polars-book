80427ece3020483f06b4d216a7b7a10dba6c652f
Liam Brannigan <l.brannigan@analyticsengines.com>
2022-05-31 11:10:22 +0100

    change to snake case

--- user_guide/src/examples/missing_data/fill_strategies.py
+++ user_guide/src/examples/missing_data/fill_strategies.py
@@ -2,13 +2,13 @@ import polars as pl
 
 df = pl.DataFrame({"col1": [1, 2, 3], "col2": [1, None, 3]})
 
-fillLiteral = df.with_column(pl.col("col2").fill_null(pl.lit(2)))
+fill_literal_df = df.with_column(pl.col("col2").fill_null(pl.lit(2)))
 
-fillForward = df.with_column(pl.col("col2").fill_null("forward"))
+fill_forward_df = df.with_column(pl.col("col2").fill_null("forward"))
 
-fillMedian = df.with_column(pl.col("col2").fill_null(pl.median("col2")))
+fill_median_df = df.with_column(pl.col("col2").fill_null(pl.median("col2")))
 
-fillInterpolation = df.with_column(pl.col("col2").interpolate())
+fill_interpolation_df = df.with_column(pl.col("col2").interpolate())
 
-dfNaN = pl.DataFrame({"col1": [1, 2, 3], "col2": [1.0, float("nan"), 3.0]})
-meanNaN = dfNaN.with_column(pl.col("col2").fill_nan(None).alias("col2")).mean()
+nan_df = pl.DataFrame({"col1": [1, 2, 3], "col2": [1.0, float("nan"), 3.0]})
+mean_nan_df = nan_df.with_column(pl.col("col2").fill_nan(None).alias("col2")).mean()
