358a45cba2beaf277fe5755be93746829fa73e07
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-01-30 10:21:53 +0000

    un-overwrite-df (#240)
    Co-authored-by: MarcoGorelli <>

--- user_guide/src/examples/time_series/parsing_dates.py
+++ user_guide/src/examples/time_series/parsing_dates.py
@@ -17,6 +17,6 @@ df_with_year = df.with_column(pl.col("Date").dt.year().alias("year"))
 
 ts = pl.Series(["-1300-05-23", "-1400-03-02"]).str.strptime(pl.Date)
 
-df = pl.DataFrame({"ts": ts, "values": [3, 4]})
+negative_dates_df = pl.DataFrame({"ts": ts, "values": [3, 4]})
 
-negative_dates_filtered_df = df.filter(pl.col("ts").dt.year() < -1300)
+negative_dates_filtered_df = negative_dates_df.filter(pl.col("ts").dt.year() < -1300)
