d513dbcc2e025e1f0ac6f95429a0fd440a64e28d
chielpeters <c.peters@theoceancleanup.com>
2023-04-20 15:49:40 +0200

    Fix paths

--- docs/src/user-guide/python/transformations/time-series/parsing.py
+++ docs/src/user-guide/python/transformations/time-series/parsing.py
@@ -1,13 +1,13 @@
 import polars as pl
 
 # --8<-- [start:df]
-df = pl.read_csv("docs/src/appleStock.csv", try_parse_dates=True)
+df = pl.read_csv("docs/src/user-guide/appleStock.csv", try_parse_dates=True)
 print(df)
 # --8<-- [end:df]
 
 
 # --8<-- [start:cast]
-df = pl.read_csv("docs/src/appleStock.csv", try_parse_dates=False)
+df = pl.read_csv("docs/src/user-guide/appleStock.csv", try_parse_dates=False)
 
 df = df.with_columns(pl.col("Date").str.strptime(pl.Date, fmt="%Y-%m-%d"))
 print(df)
