8f79ad4a9d57d9f37548f9ffc9f73338738c1866
chielpeters <c.peters@theoceancleanup.com>
2023-04-26 17:31:17 +0200

    rust examples io

--- docs/src/python/user-guide/io/database.py
+++ docs/src/python/user-guide/io/database.py
@@ -2,9 +2,16 @@
 # --8<-- [start:read]
 import polars as pl
 
-conn = "postgres://username:password@server:port/database"
+uri = "postgres://username:password@server:port/database"
 query = "SELECT * FROM foo"
 
-pl.read_sql(query, conn)
+pl.read_database(query=query, uri=uri)
 # --8<-- [end:read]
+
+# --8<-- [start:adbc]
+uri = "postgres://username:password@server:port/database"
+query = "SELECT * FROM foo"
+
+pl.read_database(query=query, uri=uri,engine="adbc")
+# --8<-- [end:adbc]
 """
