60b1aa6b8b5ca1a55a08e0c234fb4d2d6a8b690a
universalmind303 <universalmind.candy@gmail.com>
2023-05-03 18:00:07 -0500

    separate "streaming" from "lazy-vs-eager"

--- docs/src/python/user-guide/concepts/lazy-vs-eager.py
+++ docs/src/python/user-guide/concepts/lazy-vs-eager.py
@@ -19,13 +19,3 @@ q = (
 df = q.collect()
 # --8<-- [end:lazy]
 
-# --8<-- [start:streaming]
-q = (
-    pl.scan_csv("docs/src/data/iris.csv")
-    .filter(pl.col("sepal_length") > 5)
-    .groupby("species")
-    .agg(pl.col("sepal_width").mean())
-)
-
-df = q.collect(streaming=True)
-# --8<-- [end:streaming]
