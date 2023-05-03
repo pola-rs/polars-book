af5269ae968bff1c3fc272a16a91dde4167ca117
universalmind303 <universalmind.candy@gmail.com>
2023-05-03 17:58:57 -0500

    docs: add more node examples

---
+++ docs/src/node/user-guide/concepts/lazy-vs-eager.js
@@ -0,0 +1,20 @@
+const pl = require('nodejs-polars')
+
+// --8<-- [start:eager]
+
+df = pl.readCSV("docs/src/data/iris.csv")
+df_small = df.filter(col("sepal_length").gt(5))
+df_agg = df_small.groupby("species").agg(col("sepal_width").mean())
+console.log(df_agg)
+// --8<-- [end:eager]
+
+// --8<-- [start:lazy]
+q = (
+    pl.scanCSV("docs/src/data/iris.csv")
+    .filter(pl.col("sepal_length").gt(5))
+    .groupby("species")
+    .agg(pl.col("sepal_width").mean())
+)
+
+df = q.collect()
+// --8<-- [end:lazy]
