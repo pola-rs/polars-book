2fdb784814dbce8f76d55f582dce7e8b09c2cefd
Cory Grinstead <universalmind.candy@gmail.com>
2023-05-04 06:57:08 -0500

    feat: more node examples (#310)
    * docs: add more node examples
    
    * separate "streaming" from "lazy-vs-eager"
    
    * fix ci issues
    
    * fix ci issues
    
    * fix ci issues
    
    * fix ci issues
    
    * fix ci issues

--- docs/src/rust/user-guide/concepts/lazy-vs-eager.rs
+++ docs/src/rust/user-guide/concepts/lazy-vs-eager.rs
@@ -21,17 +21,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
     println!("{}", df);
     // --8<-- [end:lazy]
 
-    // --8<-- [start:streaming]
-    let q = LazyCsvReader::new("docs/src/data/iris.csv")
-        .has_header(true)
-        .finish()?
-        .filter(col("sepal_length").gt(lit(5)))
-        .groupby(vec![col("species")])
-        .agg([col("sepal_width").mean()]);
-    
-    let df = q.with_streaming(true).collect()?;
-    println!("{}", df);
-    // --8<-- [end:streaming]
 
     Ok(())
 }
