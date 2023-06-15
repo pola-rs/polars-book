c93e9faad3c92d334b24cb877c7e4f244f88fa4c
Avinash Sudesh Mallya <avimallu@Avinashs-Laptop.local>
2023-06-15 13:20:46 -0500

    initial comprehensive list tutorial

--- docs/src/rust/user-guide/expressions/lists.rs
+++ docs/src/rust/user-guide/expressions/lists.rs
@@ -1,52 +1,53 @@
 use polars::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
-    // --8<-- [start:dataframe]
-    let grades = df!(
-        "student" => &["bas", "laura", "tim", "jenny"],
-        "arithmetic" => &[10, 5, 6, 8],
-        "biology" => &[4, 6, 2, 7],
-        "geography" => &[8, 4, 9, 7],
+
+    // --8<-- [start:weather_df]
+    let weather = df!(
+        "Station" => &[
+        "Station 1", "Station 2",
+        "Station 3", "Station 4",
+        "Station 5"],
+        "Temperatures" => &[
+        "20 5 5 E1 7 13 19 9 6 20",
+        "18 8 16 11 23 E2 8 E2 E2 E2 90 70 40",
+        "19 24 E9 16 6 12 10 22",
+        "E2 E0 15 7 8 10 E1 24 17 13 6",
+        "14 8 E0 16 22 24 E1"]
     )?;
-    println!("{}", grades);
-    // --8<-- [end:dataframe]
+    println!("{}", weather);
+    // --8<-- [end:weather_df]
+    
+    // --8<-- [start:string_to_list]
+    // to do
+    // --8<-- [end:string_to_list]
 
-    // --8<-- [start:rank]
-    let out = grades
-        .clone()
-        .lazy()
-        .select([concat_lst([all().exclude(["student"])]).alias("all_grades")])
-        .collect()?;
-    println!("{}", out);
-    // --8<-- [end:rank]
+    // --8<-- [start:explode_to_atomic]
+    // to do
+    // --8<-- [end:explode_to_atomic]
+    
+    // --8<-- [start:list_ops]
+    // to do
+    // --8<-- [end:list_ops]
 
-    // --8<-- [start:expression]
-    // the percentage rank expression
-    let rank_opts = RankOptions {
-        method: RankMethod::Average,
-        descending: true,
-    };
-    let rank_pct = col("").rank(rank_opts) / col("").count().cast(DataType::Float32);
+    // --8<-- [start:count_errors]
+    // to do
+    // --8<-- [end:count_errors]
 
-    let grades = grades
-        .clone()
-        .lazy()
-        .with_columns(
-            // create the list of homogeneous data
-            [concat_lst([all().exclude(["student"])]).alias("all_grades")],
-        )
-        .select([
-            // select all columns except the intermediate list
-            all().exclude(["all_grades"]),
-            // compute the rank by calling `arr.eval`
-            col("all_grades")
-                .arr()
-                .eval(rank_pct, true)
-                .alias("grades_rank"),
-        ])
-        .collect()?;
-    println!("{}", grades);
-    // --8<-- [end:expression]
-    Ok(())
+    // --8<-- [start:count_errors_regex]
+    // to do
+    // --8<-- [end:count_errors_regex]
+
+    // --8<-- [start:weather_by_day]
+    // to do
+    // --8<-- [end:weather_by_day]
+
+    // --8<-- [start:weather_by_day_rank]
+    // to do
+    // --8<-- [end:weather_by_rank]
+
+    // --8<-- [start:array_df]
+    // to do
+    // --8<-- [end:array_df]
+
 }
