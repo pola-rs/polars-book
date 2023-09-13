8193e78b2999b5329dc47d456254194e38518ab1
noam-delfina <126973819+noam-delfina@users.noreply.github.com>
2023-09-13 03:32:49 -0400

    Add documentation for writing polars dataframes to bigquery (#391)
    Co-authored-by: Stijn de Gooijer <stijn@degooijer.io>

--- docs/src/python/user-guide/io/bigquery.py
+++ docs/src/python/user-guide/io/bigquery.py
@@ -15,4 +15,24 @@ rows = query_job.result()  # Waits for query to finish
 
 df = pl.from_arrow(rows.to_arrow())
 # --8<-- [end:read]
+
+# --8<-- [start:write]
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+# Write dataframe to stream as parquet file; does not hit disk
+with io.BytesIO() as stream:
+    df.write_parquet(stream)
+    stream.seek(0)
+    job = client.load_table_from_file(
+        stream,
+        destination='tablename',
+        project='projectname',
+        job_config=bigquery.LoadJobConfig(
+            source_format=bigquery.SourceFormat.PARQUET,
+        ),
+    )
+job.result()  # Waits for the job to complete
+# --8<-- [end:write]
 """
