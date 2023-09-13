pola-rs/polars-book

commit 7a51accc721497db0f508d3f81c702dbedf9be5d

Update code examples for `0.19` (#390)

docs/src/rust/getting-started/expressions.rs
@@ -1,44 +1,53 @@
+use chrono::prelude::*;
 use polars::prelude::*;
 use rand::Rng;
-use chrono::prelude::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut rng = rand::thread_rng();
 
     let df: DataFrame = df!("a" => 0..8,
-                            "b"=> (0..8).map(|_| rng.gen::<f64>()).collect::<Vec<f64>>(),
-                            "c"=> [
-                                NaiveDate::from_ymd_opt(2022, 12, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                                NaiveDate::from_ymd_opt(2022, 12, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                                NaiveDate::from_ymd_opt(2022, 12, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                                NaiveDate::from_ymd_opt(2022, 12, 4).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                                NaiveDate::from_ymd_opt(2022, 12, 5).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                                NaiveDate::from_ymd_opt(2022, 12, 6).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                                NaiveDate::from_ymd_opt(2022, 12, 7).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                                NaiveDate::from_ymd_opt(2022, 12, 8).unwrap().and_hms_opt(0, 0, 0).unwrap(),
-                            ],
-                            "d"=> [Some(1.0), Some(2.0), None, None, Some(0.0), Some(-5.0), Some(-42.), None]
-                        ).expect("should not fail");
-    
+        "b"=> (0..8).map(|_| rng.gen::<f64>()).collect::<Vec<f64>>(),
+        "c"=> [
+            NaiveDate::from_ymd_opt(2022, 12, 1).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2022, 12, 2).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2022, 12, 3).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2022, 12, 4).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2022, 12, 5).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2022, 12, 6).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2022, 12, 7).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+            NaiveDate::from_ymd_opt(2022, 12, 8).unwrap().and_hms_opt(0, 0, 0).unwrap(),
+        ],
+        "d"=> [Some(1.0), Some(2.0), None, None, Some(0.0), Some(-5.0), Some(-42.), None]
+    )
+    .expect("should not fail");
+
     // --8<-- [start:select]
     let out = df.clone().lazy().select([col("*")]).collect()?;
-    println!("{}",out);
+    println!("{}", out);
     // --8<-- [end:select]
 
     // --8<-- [start:select2]
     let out = df.clone().lazy().select([col("a"), col("b")]).collect()?;
-    println!("{}",out);
+    println!("{}", out);
     // --8<-- [end:select2]
 
     // --8<-- [start:select3]
-    let out = df.clone().lazy().select([col("a"), col("b")]).limit(3).collect()?;
-    println!("{}",out);
+    let out = df
+        .clone()
+        .lazy()
+        .select([col("a"), col("b")])
+        .limit(3)
+        .collect()?;
+    println!("{}", out);
     // --8<-- [end:select3]
 
     // --8<-- [start:exclude]
-    let out = df.clone().lazy().select([col("*").exclude(["a"])]).collect()?;
-    println!("{}",out);
+    let out = df
+        .clone()
+        .lazy()
+        .select([col("*").exclude(["a"])])
+        .collect()?;
+    println!("{}", out);
     // --8<-- [end:exclude]
 
     // --8<-- [start:filter]
@@ -59,62 +68,77 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                 .and(col("c").lt_eq(lit(end_date))),
         )
         .collect()?;
-    println!("{}",out);
+    println!("{}", out);
     // --8<-- [end:filter]
 
     // --8<-- [start:filter2]
-    let out = df.clone().lazy().filter(col("a").lt_eq(3).and(col("d").is_not_null())).collect()?;
-    println!("{}",out);
+    let out = df
+        .clone()
+        .lazy()
+        .filter(col("a").lt_eq(3).and(col("d").is_not_null()))
+        .collect()?;
+    println!("{}", out);
     // --8<-- [end:filter2]
 
     // --8<-- [start:with_columns]
-    let out = df.clone().lazy().with_columns(
-        [
+    let out = df
+        .clone()
+        .lazy()
+        .with_columns([
             col("b").sum().alias("e"),
-            (col("b") + lit(42)).alias("b+42")
-        ]
-    ).collect()?;
-    println!("{}",out);
+            (col("b") + lit(42)).alias("b+42"),
+        ])
+        .collect()?;
+    println!("{}", out);
     // --8<-- [end:with_columns]
 
     // --8<-- [start:dataframe2]
     let df2: DataFrame = df!("x" => 0..8,
-                            "y"=> &["A", "A", "A", "B", "B", "C", "X", "X"],
-                        ).expect("should not fail");
-    println!("{}",df2);
+        "y"=> &["A", "A", "A", "B", "B", "C", "X", "X"],
+    )
+    .expect("should not fail");
+    println!("{}", df2);
     // --8<-- [end:dataframe2]
 
-    // --8<-- [start:groupby]
-    let out = df2.clone().lazy().groupby(["y"]).agg([count()]).collect()?;
-    println!("{}",out);
-    // --8<-- [end:groupby]
+    // --8<-- [start:group_by]
+    let out = df2
+        .clone()
+        .lazy()
+        .group_by(["y"])
+        .agg([count()])
+        .collect()?;
+    println!("{}", out);
+    // --8<-- [end:group_by]
 
-    // --8<-- [start:groupby2]
-    let out = df2.clone().lazy().groupby(["y"]).agg([
-        col("*").count().alias("count"),
-        col("*").sum().alias("sum"),
-    ]).collect()?;
-    println!("{}",out);
-    // --8<-- [end:groupby2]
+    // --8<-- [start:group_by2]
+    let out = df2
+        .clone()
+        .lazy()
+        .group_by(["y"])
+        .agg([col("*").count().alias("count"), col("*").sum().alias("sum")])
+        .collect()?;
+    println!("{}", out);
+    // --8<-- [end:group_by2]
 
     // --8<-- [start:combine]
-    let out = df.clone().lazy().with_columns([
-        (col("a") * col("b")).alias("a * b")
-    ]).select([
-        col("*").exclude(["c","d"])
-    ]).collect()?;
-    println!("{}",out);
+    let out = df
+        .clone()
+        .lazy()
+        .with_columns([(col("a") * col("b")).alias("a * b")])
+        .select([col("*").exclude(["c", "d"])])
+        .collect()?;
+    println!("{}", out);
     // --8<-- [end:combine]
 
     // --8<-- [start:combine2]
-    let out = df.clone().lazy().with_columns([
-        (col("a") * col("b")).alias("a * b")
-    ]).select([
-        col("*").exclude(["d"])
-    ]).collect()?;
-    println!("{}",out);
+    let out = df
+        .clone()
+        .lazy()
+        .with_columns([(col("a") * col("b")).alias("a * b")])
+        .select([col("*").exclude(["d"])])
+        .collect()?;
+    println!("{}", out);
     // --8<-- [end:combine2]
-    
-    Ok(())
 
+    Ok(())
 }

docs/src/rust/home/example.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .has_header(true)
         .finish()?
         .filter(col("sepal_length").gt(lit(5)))
-        .groupby(vec![col("species")])
+        .group_by(vec![col("species")])
         .agg([col("*").sum()]);
 
     let df = q.collect();

docs/src/rust/user-guide/concepts/contexts.rs
@@ -1,11 +1,9 @@
 use polars::prelude::*;
 
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
     // --8<-- [start:dataframe]
     use rand::{thread_rng, Rng};
-    
+
     let mut arr = [0f64; 5];
     thread_rng().fill(&mut arr);
 
@@ -19,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{}", &df);
     // --8<-- [end:dataframe]
 
-// --8<-- [start:select]
+    // --8<-- [start:select]
     let out = df
         .clone()
         .lazy()
@@ -31,14 +29,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ])
         .collect()?;
     println!("{}", out);
-// --8<-- [end:select]
+    // --8<-- [end:select]
 
-// --8<-- [start:filter]
+    // --8<-- [start:filter]
     let out = df.clone().lazy().filter(col("nrs").gt(lit(2))).collect()?;
     println!("{}", out);
-// --8<-- [end:filter]
+    // --8<-- [end:filter]
 
-// --8<-- [start:with_columns]
+    // --8<-- [start:with_columns]
     let out = df
         .clone()
         .lazy()
@@ -48,12 +46,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ])
         .collect()?;
     println!("{}", out);
-// --8<-- [end:with_columns]
+    // --8<-- [end:with_columns]
 
-    // --8<-- [start:groupby]
+    // --8<-- [start:group_by]
     let out = df
         .lazy()
-        .groupby([col("groups")])
+        .group_by([col("groups")])
         .agg([
             sum("nrs"),                           // sum nrs by groups
             col("random").count().alias("count"), // count group members
@@ -66,6 +64,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ])
         .collect()?;
     println!("{}", out);
-    // --8<-- [end:groupby]
+    // --8<-- [end:group_by]
     Ok(())
 }

docs/src/rust/user-guide/concepts/lazy-vs-eager.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mask = df.column("sepal_length")?.f64()?.gt(5.0);
     let df_small = df.filter(&mask)?;
     let df_agg = df_small
-        .groupby(["species"])?
+        .group_by(["species"])?
         .select(["sepal_width"])
         .mean()?;
     println!("{}", df_agg);
@@ -20,7 +20,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .has_header(true)
         .finish()?
         .filter(col("sepal_length").gt(lit(5)))
-        .groupby(vec![col("species")])
+        .group_by(vec![col("species")])
         .agg([col("sepal_width").mean()]);
     let df = q.collect()?;
     println!("{}", df);

docs/src/rust/user-guide/concepts/streaming.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .has_header(true)
         .finish()?
         .filter(col("sepal_length").gt(lit(5)))
-        .groupby(vec![col("species")])
+        .group_by(vec![col("species")])
         .agg([col("sepal_width").mean()]);
 
     let df = q.with_streaming(true).collect()?;

docs/src/rust/user-guide/expressions/aggregation.rs
@@ -1,10 +1,9 @@
 use polars::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    
     // --8<-- [start:dataframe]
-    use std::io::Cursor;
     use reqwest::blocking::Client;
+    use std::io::Cursor;
 
     let url = "https://theunitedstates.io/congress-legislators/legislators-historical.csv";
 
@@ -31,7 +30,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .groupby(["first_name"])
+        .group_by(["first_name"])
         .agg([count(), col("gender").list(), col("last_name").first()])
         .sort(
             "count",
@@ -50,7 +49,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .groupby(["state"])
+        .group_by(["state"])
         .agg([
             (col("party").eq(lit("Anti-Administration")))
                 .sum()
@@ -76,7 +75,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .groupby(["state", "party"])
+        .group_by(["state", "party"])
         .agg([col("party").count().alias("count")])
         .filter(
             col("party")
@@ -111,7 +110,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .groupby(["state"])
+        .group_by(["state"])
         .agg([
             avg_birthday("M"),
             avg_birthday("F"),
@@ -139,7 +138,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 nulls_last: true,
             },
         )
-        .groupby(["state"])
+        .group_by(["state"])
         .agg([
             get_person().first().alias("youngest"),
             get_person().last().alias("oldest"),
@@ -161,7 +160,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 nulls_last: true,
             },
         )
-        .groupby(["state"])
+        .group_by(["state"])
         .agg([
             get_person().first().alias("youngest"),
             get_person().last().alias("oldest"),
@@ -184,7 +183,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 nulls_last: true,
             },
         )
-        .groupby(["state"])
+        .group_by(["state"])
         .agg([
             get_person().first().alias("youngest"),
             get_person().last().alias("oldest"),

docs/src/rust/user-guide/expressions/user-defined-functions.rs
@@ -1,7 +1,6 @@
 use polars::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     // --8<-- [start:dataframe]
     let df = df!(
         "keys" => &["a", "a", "b"],
@@ -10,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let out = df
         .lazy()
-        .groupby(["keys"])
+        .group_by(["keys"])
         .agg([
             col("values")
                 .map(|s| Ok(s.shift(1)), GetOutput::default())
@@ -26,7 +25,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out = df
         .clone()
         .lazy()
-        .groupby([col("keys")])
+        .group_by([col("keys")])
         .agg([
             col("values")
                 .apply(|s| Ok(s.shift(1)), GetOutput::default())
@@ -38,7 +37,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --8<-- [end:apply]
 
     // --8<-- [start:counter]
-    
+
     // --8<-- [end:counter]
 
     // --8<-- [start:combine]

docs/_build/API_REFERENCE_LINKS.yml
@@ -5,7 +5,7 @@ python:
   select: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.select.html
   filter: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.filter.html
   with_columns: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.with_columns.html
-  groupby: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.groupby.html
+  group_by: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.group_by.html
   join: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.join.html
   hstack: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.hstack.html
   read_csv: https://pola-rs.github.io/polars/py-polars/html/reference/api/polars.read_csv.html
@@ -144,7 +144,7 @@ python:
     feature_flags: ['timezone']
   date_range: https://pola-rs.github.io/polars/py-polars/html/reference/api/polars.date_range.html
   upsample: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.upsample.html
-  groupby_dynamic: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.groupby_dynamic.html
+  group_by_dynamic: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.group_by_dynamic.html
   explode: https://pola-rs.github.io/polars/py-polars/html/reference/dataframe/api/polars.DataFrame.explode.html
   cast: https://pola-rs.github.io/polars/py-polars/html/reference/expressions/api/polars.Expr.cast.html
   np.log:
@@ -175,7 +175,7 @@ rust:
   select: https://pola-rs.github.io/polars/docs/rust/dev/polars_lazy/frame/struct.LazyFrame.html#method.select
   filter: https://pola-rs.github.io/polars/docs/rust/dev/polars_lazy/frame/struct.LazyFrame.html#method.filter
   with_columns: https://pola-rs.github.io/polars/docs/rust/dev/polars_lazy/frame/struct.LazyFrame.html#method.with_columns
-  groupby: https://pola-rs.github.io/polars/docs/rust/dev/polars_lazy/frame/struct.LazyFrame.html#method.groupby
+  group_by: https://pola-rs.github.io/polars/docs/rust/dev/polars_lazy/frame/struct.LazyFrame.html#method.group_by
   join: https://pola-rs.github.io/polars/docs/rust/dev/polars_core/frame/hash_join/index.html
   hstack: https://pola-rs.github.io/polars/docs/rust/dev/polars_core/frame/struct.DataFrame.html#method.hstack
   SQLContext: https://pola-rs.github.io/polars/py-polars/html/reference/sql.html

commit 97e8a010159904d17a62408481d6beee5d24629c

Rename `group_by` in rust code to `groupby`
Fixes https://github.com/pola-rs/polars-book/issues/400

Bug started from: https://github.com/pola-rs/polars-book/commit/7a51accc721497db0f508d3f81c702dbedf9be5d

docs/src/rust/getting-started/expressions.rs
@@ -101,12 +101,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --8<-- [end:dataframe2]
 
     // --8<-- [start:group_by]
-    let out = df2
-        .clone()
-        .lazy()
-        .group_by(["y"])
-        .agg([count()])
-        .collect()?;
+    let out = df2.clone().lazy().groupby(["y"]).agg([count()]).collect()?;
     println!("{}", out);
     // --8<-- [end:group_by]
 
@@ -114,7 +109,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out = df2
         .clone()
         .lazy()
-        .group_by(["y"])
+        .groupby(["y"])
         .agg([col("*").count().alias("count"), col("*").sum().alias("sum")])
         .collect()?;
     println!("{}", out);

docs/src/rust/home/example.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .has_header(true)
         .finish()?
         .filter(col("sepal_length").gt(lit(5)))
-        .group_by(vec![col("species")])
+        .groupby(vec![col("species")])
         .agg([col("*").sum()]);
 
     let df = q.collect();

docs/src/rust/user-guide/concepts/contexts.rs
@@ -51,7 +51,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // --8<-- [start:group_by]
     let out = df
         .lazy()
-        .group_by([col("groups")])
+        .groupby([col("groups")])
         .agg([
             sum("nrs"),                           // sum nrs by groups
             col("random").count().alias("count"), // count group members

docs/src/rust/user-guide/concepts/lazy-vs-eager.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mask = df.column("sepal_length")?.f64()?.gt(5.0);
     let df_small = df.filter(&mask)?;
     let df_agg = df_small
-        .group_by(["species"])?
+        .groupby(["species"])?
         .select(["sepal_width"])
         .mean()?;
     println!("{}", df_agg);
@@ -20,7 +20,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .has_header(true)
         .finish()?
         .filter(col("sepal_length").gt(lit(5)))
-        .group_by(vec![col("species")])
+        .groupby(vec![col("species")])
         .agg([col("sepal_width").mean()]);
     let df = q.collect()?;
     println!("{}", df);

docs/src/rust/user-guide/concepts/streaming.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .has_header(true)
         .finish()?
         .filter(col("sepal_length").gt(lit(5)))
-        .group_by(vec![col("species")])
+        .groupby(vec![col("species")])
         .agg([col("sepal_width").mean()]);
 
     let df = q.with_streaming(true).collect()?;

docs/src/rust/user-guide/expressions/aggregation.rs
@@ -30,7 +30,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .group_by(["first_name"])
+        .groupby(["first_name"])
         .agg([count(), col("gender").list(), col("last_name").first()])
         .sort(
             "count",
@@ -49,7 +49,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .group_by(["state"])
+        .groupby(["state"])
         .agg([
             (col("party").eq(lit("Anti-Administration")))
                 .sum()
@@ -75,7 +75,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .group_by(["state", "party"])
+        .groupby(["state", "party"])
         .agg([col("party").count().alias("count")])
         .filter(
             col("party")
@@ -110,7 +110,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let df = dataset
         .clone()
         .lazy()
-        .group_by(["state"])
+        .groupby(["state"])
         .agg([
             avg_birthday("M"),
             avg_birthday("F"),
@@ -138,7 +138,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 nulls_last: true,
             },
         )
-        .group_by(["state"])
+        .groupby(["state"])
         .agg([
             get_person().first().alias("youngest"),
             get_person().last().alias("oldest"),
@@ -160,7 +160,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 nulls_last: true,
             },
         )
-        .group_by(["state"])
+        .groupby(["state"])
         .agg([
             get_person().first().alias("youngest"),
             get_person().last().alias("oldest"),
@@ -183,7 +183,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 nulls_last: true,
             },
         )
-        .group_by(["state"])
+        .groupby(["state"])
         .agg([
             get_person().first().alias("youngest"),
             get_person().last().alias("oldest"),

docs/src/rust/user-guide/expressions/user-defined-functions.rs
@@ -9,7 +9,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let out = df
         .lazy()
-        .group_by(["keys"])
+        .groupby(["keys"])
         .agg([
             col("values")
                 .map(|s| Ok(s.shift(1)), GetOutput::default())
@@ -25,7 +25,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out = df
         .clone()
         .lazy()
-        .group_by([col("keys")])
+        .groupby([col("keys")])
         .agg([
             col("values")
                 .apply(|s| Ok(s.shift(1)), GetOutput::default())